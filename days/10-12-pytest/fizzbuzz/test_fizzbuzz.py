5c4cdf15ee7cd24bc9f284c0a9a8cb486caa853d
Ninad <superninad@yahoo.co.in>
2024-12-19 22:56:39 -0800

    Changed to unittest

--- days/10-12-pytest/fizzbuzz/test_fizzbuzz.py
+++ days/10-12-pytest/fizzbuzz/test_fizzbuzz.py
@@ -1,24 +1,12 @@
-import pytest
+import unittest
 
 from fizzbuzz import fizzbuzz
 
-@pytest.mark.parametrize("arg, ret",[
-    (1, 1),
-    (2, 2),
-    (3, 'Fizz'),
-    (4, 4),
-    (5, 'Buzz'),
-    (6, 'Fizz'),
-    (7, 7),
-    (8, 8),
-    (9, 'Fizz'),
-    (10, 'Buzz'),
-    (11, 11),
-    (12, 'Fizz'),
-    (13, 13),
-    (14, 14),
-    (15, 'Fizz Buzz'),
-    (16, 16),
-])
-def test_fizzbuzz(arg, ret):
-    assert fizzbuzz(arg) == ret
+class TestFizzBuzz(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual(fizzbuzz(1),1)
+        self.assertEqual(fizzbuzz(2),2)
+        self.assertEqual(fizzbuzz(3), "Fizz")
+
+if __name__ == "__main__":
+    unittest.main()
